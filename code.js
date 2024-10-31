c9a9409c7f7bc5351ed3839ce58c65883578ec0a
Nabil <rousani@gmail.com>
2024-10-31 09:36:38 -0700

    visibility examples

--- code.js
+++ code.js
@@ -1,11 +1,5 @@
-x = document.createElement("p")
-x.innerHTML = `
-  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo hic ea ab laudantium, quisquam, dolores labore, voluptate atque odit commodi aliquam sit quia reprehenderit. Esse laborum autem quibusdam at atque.
-`
-x.addEventListener("click", () => {
-  console.log(111);
-})
-
-y = document.getElementById("b1")
-console.log(y);
-y.appendChild(x)
+x = document.getElementById("p1")
+// x.remove()
+x.style.display = "none"
+x.style.display = "block"
+// x.style.visibility = "hidden"
