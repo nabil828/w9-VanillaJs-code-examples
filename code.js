097e943975d030144c43e803f66915132ff5af4f
Nabil <rousani@gmail.com>
2024-10-31 09:18:49 -0700

    createElement example

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
-jQuery("btn1").click()
+x = document.createElement("p")
+x.innerHTML = `
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo hic ea ab laudantium, quisquam, dolores labore, voluptate atque odit commodi aliquam sit quia reprehenderit. Esse laborum autem quibusdam at atque.
+`
+x.addEventListener("click", () => {
+  console.log(111);
+})
+
+y = document.getElementById("b1")
+console.log(y);
+y.appendChild(x)
