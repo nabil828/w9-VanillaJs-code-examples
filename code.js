4b28b034d1b1ef0d1247fbf44e871e798c47ec2c
Nabil <rousani@gmail.com>
2024-10-31 08:56:37 -0700

    innerHTML example

--- code.js
+++ code.js
@@ -1,14 +1,4 @@
-console.log(111)
 
 x = document.getElementById("p1");
-y = document.getElementsByTagName("p");
-z = document.getElementsByClassName("c1");
-
-// same as css selectors 
-x_ = document.querySelectorAll("#p1");
-y_ = document.querySelectorAll("p");
-y_ = document.querySelectorAll(".c1");
-
-
-y__ = document.querySelector(".c1");
-console.log(y__);
+x.innerHTML = "This is a <b>paragraph</b>"
+console.log(x.innerHTML);
