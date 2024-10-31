5a1cfb963ad39f56daf5c97beaf594265486d136
Nabil <rousani@gmail.com>
2024-10-31 09:41:13 -0700

    classList example

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 x = document.getElementById("p1")
-// x.remove()
-x.style.display = "none"
-x.style.display = "block"
-// x.style.visibility = "hidden"
+x.classList.add("tomato")
+// x.classList.remove("tomato")
+x.classList.toggle("tomato")
+console.log(x.classList);
