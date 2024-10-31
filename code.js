509902e2b76cb91f45992d834305076069d69525
Nabil <rousani@gmail.com>
2024-10-31 10:02:14 -0700

    fetch example

--- code.js
+++ code.js
@@ -1,5 +1,28 @@
-x = document.getElementById("p1")
-x.classList.add("tomato")
-// x.classList.remove("tomato")
-x.classList.toggle("tomato")
-console.log(x.classList);
+// fetch("https://yts.mx/api/v2/list_movies.json")
+//   .then(
+//     (resp) => {
+//       return resp.json()
+//     }
+//   )
+//   .then(
+//     (input) => {
+//       console.log(input);
+//     }
+//   )
+//   .catch(
+//     (err) => {
+//       console.log(err);
+//     }
+//   )
+
+fetch("https://yts.mx/api/v2/list_movies.json")
+  .then(resp => resp.json())
+  .then(resp => { console.log(resp) })
+  .catch(err => { console.log(err) }
+  )
+
+
+// $.ajax({
+//   "url": "https://yts.mx/api/v2/list_movies.json",
+//   success: f1
+// })
